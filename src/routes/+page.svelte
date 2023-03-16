97d03579790023b65ac94defed580ab79701635b
NBadeloe <48018083+NBadeloe@users.noreply.github.com>
2023-03-16 14:08:17 +0100

    final commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    import HeaderV2 from "cmd-midterm-components/components/HeaderV2.svelte";
+    import ChapterContent from "cmd-midterm-components/components/ChapterContent.svelte";
+
+    import Carousel from "cmd-midterm-components/components/Carousel.svelte";
+    import { Footer } from "cmd-midterm-components";
+   
+
+</script>
+
+<body>
+    <HeaderV2 />
+    
+    <ChapterContent />
+    <Carousel />
+
+    <Footer />
+</body>
+
+<style>
+    body{
+        margin: 0.4em 2.5em;
+    }
+</style>
+
+   
